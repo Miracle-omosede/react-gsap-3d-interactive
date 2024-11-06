81073c4e3ed04b6e94d8a6f93774f803fd6cb017
Miracle-omosede <miracleigbinoghene@gmail.com>
2024-11-06 09:48:22 +0100

    changed the header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,29 @@
 import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { heroVideo, smallHeroVideo } from "../utils"
+import { useState, useEffect } from "react"
 
 const Hero = () => {
+    const [videoSrc , setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+
+    const handleVideoSrcSet = () => {
+        if(window.innerWidth < 760) {
+            setVideoSrc(smallHeroVideo)
+        } else {
+            setVideoSrc(heroVideo)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleVideoSrcSet)
+      return () => {
+        window.removeEventListener('resize', handleVideoSrcSet)
+      }
+    }, [])
+    
     useGSAP(()=>{
         gsap.to("#hero",{ opacity: 1, delay:1.5 })
+        gsap.to("#cta",{ opacity: 1, y: -50, delay: 2})
     },
     [])
   return (
@@ -13,10 +32,15 @@ const Hero = () => {
             <p id="hero" className="hero-title">iphone 15 Pro</p>
 
             <div className="md:w-10/12 w-9/12">
-                <video>
+                <video  className="pointer-events-none" autoPlay muted playsInline={true} key={videoSrc}>
                     <source src={videoSrc} />
                 </video>
             </div>
+        </div>
+
+        <div id="cta" className="flex flex-col items-center opacity-0 translate-y-20">
+            <a href="#highlight" className="btn">Buy</a>
+            <p className="font-normal text-xl">From $199/month or $999</p>
         </div>
     </section>
   )
